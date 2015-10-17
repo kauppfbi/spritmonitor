9d634bb46debe6ae5059353a88cf69b0a3750543
kauppfbi <fabian.kaupp@mail.de>
2015-10-17 22:57:00 +0200

    modifizierte Authentifizierungsstrategie
    User werden in einem json file gespeichert;
    
    config funktioniert noch nicht einwandfrei!!!
    
    um den Passport zu nutzen, muss er in www.js eingelesen werden

--- developement/app/models/user.js
+++ developement/app/models/user.js
@@ -1,40 +1,101 @@
 var bcrypt = require('bcrypt-nodejs');
+var jsonfile = require('jsonfile');
+var fs = require('fs');
+var util = require('util');
+ 
+var file = '../../data/user.json';
 
-// define the schema for our user model
-var userSchema = {
-	local            : {
-        email        : 'String',
-        password     : 'String',
-    },
-    facebook         : {
-        id           : 'String',
-        token        : 'String',
-        email        : 'String',
-        name         : 'String'
-    },
-    twitter          : {
-        id           : 'String',
-        token        : 'String',
-        displayName  : 'String',
-        username     : 'String'
-    },
-    google           : {
-        id           : 'String',
-        token        : 'String',
-        email        : 'String',
-        name         : 'String'
-    }};
 
-// generating a hash
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+/*
+
+	generateHash = function(password) {
+		return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+	};
+
+	validPassword = function(password) {
+		return bcrypt.compareSync(password, this.local.password);
+	};
+*/
+
+var findById = function (id) {};
+
+/*
+* returns true if email is still available
+* returns false if email is reserved for another user 
+*/
+var checkMail = function (email){
+	var obj = jsonfile.readFileSync(file);
+	var user = obj.user;
+
+	for (var i = 0; i < user.length; i++){
+		if (user.email == email){
+			return false;
+		}
+	}
+	return true;
 };
 
-// checking if password is valid
-userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
+var createUser = function (email, password){
+	var obj = jsonfile.readFileSync(file);
+	var user = obj.user;
+
+	var newUser = {};
+	newUser.email = email;
+	newUser.password = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+
+	user.push(newUser);
+	obj.user = user;
+
+	fs.writeFile(file, JSON.stringify(obj, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("JSON saved to " + file);
+    }
+}); 
+
 };
 
+var checkPassword = function (email, password){
+	var obj = jsonfile.readFileSync(file);
+	var user = obj.user;
 
-//userschema exportieren, evtl neue api finden 
-//module.exports = 
+	for (var i = 0; i < user.length; i++){
+		if (user.email == email){
+			return bcrypt.compareSync(user.password, password);
+		}
+	}
+};
+
+var deleteUser = function (id) {};
+
+
+exports.checkMail = checkMail;
+exports.checkPassword = checkPassword; 
+exports.createUser = createUser; 
+exports.deleteUser = deleteUser;
+exports.findById = findById; 
+
+
+/*
+var obj = jsonfile.readFileSync(file);
+
+var user = obj.user;
+
+user.push({name: "Martin", email: "mail@asdf"});
+
+obj.user = user;
+ 
+console.log(obj.user);
+
+var outputFilename = './data/user.json';
+
+fs.writeFile(outputFilename, JSON.stringify(obj, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("JSON saved to " + outputFilename);
+    }
+}); 
+*/
+
--- developement/config/passport.js
+++ developement/config/passport.js
@@ -2,6 +2,99 @@
 
 // load all the things we need
 var LocalStrategy   = require('passport-local').Strategy;
+var bcrypt = require('bcrypt');
+// load up the user model
+var User            = require('../app/models/user');
+
+// expose this function to our app using module.exports
+module.exports = function(passport) {
+
+    // =========================================================================
+    // passport session setup ==================================================
+    // =========================================================================
+    // required for persistent login sessions
+    // passport needs ability to serialize and unserialize users out of session
+
+    // used to serialize the user for the session
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    // used to deserialize the user
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
+    // =========================================================================
+    // LOCAL SIGNUP ============================================================
+    // =========================================================================
+    // we are using named strategies since we have one for login and one for signup
+    // by default, if there was no name, it would just be called 'local'
+
+    passport.use('local-signup', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) {
+
+        // asynchronous
+        // User.findOne wont fire unless data is sent back
+        process.nextTick(function() {
+
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        if (!User.checkMail(email)){
+            return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+        }
+        else {
+            User.createUser(email, password);
+        }
+
+    }});
+
+    // =========================================================================
+    // LOCAL LOGIN =============================================================
+    // =========================================================================
+    // we are using named strategies since we have one for login and one for signup
+    // by default, if there was no name, it would just be called 'local'
+
+    passport.use('local-login', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) { // callback with email and password from our form
+
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+
+        if (!User.checkMail(email)){
+             return done(null, false, req.flash('loginMessage', 'No user found.'));
+        }
+        else {
+            //user found --> check pw 
+            if (!User.checkPassword(email, password)){
+               //if password is wrong
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+            }
+            else {
+                // all is well, return successful user
+                return done(null, user);
+            }
+        }
+    });
+};
+
+/*
+// config/passport.js
+
+// load all the things we need
+var LocalStrategy   = require('passport-local').Strategy;
 
 // load up the user model
 var User            = require('../app/models/user');
@@ -115,4 +208,4 @@ module.exports = function(passport) {
     }));
 
 };
-
+*/
