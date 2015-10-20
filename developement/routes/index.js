8f2b5487cd45269ccad257e188eb7cabde36a863
kauppfbi <fabian.kaupp@mail.de>
2015-10-20 15:29:46 +0200

    Löschen der letzten node_Module

--- developement/routes/index.js
+++ developement/routes/index.js
@@ -1,8 +1,12 @@
-var express = require('express');
+
+
+var express= require('express');
+console.log(express);
 var router = express.Router();
 
-router.get('/', function(req, res){
-	res.render('index');
-});
+	router.get('/', function(req, res){
+		res.render('index');
+	});
+
 
 module.exports = router;
