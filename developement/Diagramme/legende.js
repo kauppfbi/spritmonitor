f8ad5b51b3d90fc1d7b89b865f8810a15c6cde27
dhjm <janis.muehlig@hotmail.de>
2015-10-21 19:15:43 +0200

    Update Diagramme
    Verlaufsdiagramm soweit fertig, inkl. Responsive Design, muss noch noch
    mit Nutzdaten befüllt werden. Kreisdiagramm Legende fertig, sind aber
    noch Änderungen notwendig

--- developement/Diagramme/legende.js
+++ developement/Diagramme/legende.js
@@ -4,11 +4,20 @@ var context = canvas.getContext("2d");
 
 
         context.fillStyle = "#505050";
-        context.fillRect (50, 150, 20, 20);
+        context.fillRect (50, 70, 10, 10);
 
         context.fillStyle = "orange";
-        context.fillRect (50, 200, 20, 20);
+        context.fillRect (50, 100, 10, 10);
     
         context.fillStyle = "#C0C0C0";
-        context.fillRect (50, 250, 20, 20);
+        context.fillRect (50, 130, 10, 10);
+    
+        context.font="15px Georgia";
+        context.fillText("Autobahn",90,80);
+    
+        context.font="15px Georgia";
+        context.fillText("Landstraße",90,110);
+    
+        context.font="15px Georgia";
+        context.fillText("Stadt",90,140);
 }
