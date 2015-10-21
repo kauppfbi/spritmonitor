f85e5e12ee7010fd6a9595b05c00d04b752bc12f
hahnfelix <fx.hahn@web.de>
2015-10-21 19:58:58 +0200

    Weitere Implementierungen in betankungen.js

--- developement/app/models/betankung.js
+++ developement/app/models/betankung.js
@@ -14,7 +14,33 @@ var findById = function (id) {
 	return user[id];
 };
 
+//Hier müssen noch die Daten aus den Fromularen übergeben werden
+var createBetankung = function (profilID, Datum, Kraftstoff, Liter, Kilometer, Vollbetankung){
+	var obj = jsonfile.readFileSync(file);
+	var betankungen = obj.betankungen;
 
-var createBetankung = funcion (id) {
-    
-}
+	var newBetankung = {};
+	newBetankung.profilID = profilID;
+    newBetankung.Datum = Datum;
+    newBetankung.Kraftstoff = Kraftstoff;
+    newBetankung.Liter = Liter;
+    newBetankung.Kilometer = Kilometer;
+    newBetankung.Vollbetanung = Vollbetankung;
+
+	betankungen.push(newBetankung);
+	obj.betankungen = betankungen;
+
+	fs.writeFile(file, JSON.stringify(obj, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("JSON saved to " + file);
+    }
+    return newBetankung;
+}); 
+};
+
+
+//Exports
+exports.createBetankung = createBetankung;
+exports.findById = findById;
