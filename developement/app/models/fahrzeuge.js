b8d32b8db42f5a6d4f37e3c952a9cc0d2a4aa191
kauppfbi <fabian.kaupp@mail.de>
2015-10-23 20:15:22 +0200

    Model für Fahrzeuge
    Daten werden in fahrzeuge.json gespeichert
    Dateizugriff wird über fahrzeuge.js geregelt!
    
    konkurrierender Zugriff muss noch geregelt werden!!!!!

---
+++ developement/app/models/fahrzeuge.js
@@ -0,0 +1,16 @@
+//in Bearbeitung
+
+var file = './data/fahrzeuge.json';
+
+var jsonfile = require('jsonfile');
+var fs = require('fs');
+var util = require('util');
+
+var findById = function(id){};
+
+//vehicle ist das aufbereitete fahrzeug-Objekt
+var createVehicle = function(vehicle){};
+
+exports.findById = findById(id);
+exports.createVehicle = createVehicle(vehicle);
+
