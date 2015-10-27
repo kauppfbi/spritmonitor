ab3f4be13268582e6a3539f3552ef84287244f06
kauppfbi <fabian.kaupp@mail.de>
2015-10-27 09:09:26 +0100

    Kleinigkeiten

--- developement/app/routes.js
+++ developement/app/routes.js
@@ -91,7 +91,7 @@ module.exports = function(app, passport){
     // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect : '/favoriten', // redirect to the secure profile section
-        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
 
