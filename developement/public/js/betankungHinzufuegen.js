c5faf9f5d578515404e2b0574573906ff6004deb
hahnfelix <fx.hahn@web.de>
2015-10-30 18:39:03 +0100

    diverse changes bezgl betankungen

--- developement/public/js/betankungHinzufuegen.js
+++ developement/public/js/betankungHinzufuegen.js
@@ -1,18 +1,20 @@
 window.onload = FahrzeugAuswahl;
-console.log(fahrzeugBeschreibungen);
 
-var FahrzeugAuswahl = function(){
-    var Beschreibung = fahrzeugBeschreibungen;
+var FahrzeugAuswahl = function(beschreibung){
+    var Beschreibung = beschreibung;
+    //var Beschreibung = fahrzeugBeschreibungen;
     var FahrzeugAuswahl = document.getElementById('Fahrzeug');
-    
+    console.log("Fahrzeugbeschreibung: "+Beschreibung);
+    /*
     while(FahrzeugAuswahl.hasChildNodes()){
         FahrzeugAuswahl.removeChild(FahrzeugAuswahl.childNodes[0]);
     }
+    */
     
     for(var i=0; i<Beschreibung.length; i++){
         var option = document.createElement('option');
-        option.text = Beschreibung;
-        option.value = Beschreibung;
+        option.text = Beschreibung[i];
+        option.value = Beschreibung[i];
         FahrzeugAuswahl.appendChild(option);
     }
 }
